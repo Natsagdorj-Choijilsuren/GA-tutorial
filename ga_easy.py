d53215f1d84ad18375cc00b4562f03bb58e94d3c
choi <lnatsagdor-choijilsuren@dentsu.co.jp>
2019-12-06 22:41:40 +0900

    genetic algorithm easy

--- ga_easy.py
+++ ga_easy.py
@@ -24,7 +24,6 @@ def initialize_population(len_population):
 
     return populations
 
-    
 
 #fitness function
 def string_fitness(string_individual):
@@ -43,7 +42,7 @@ def mutate_string(string_individual):
     ipos = random.randint(0, len(TARGET_STRING)-1)
 
     rchar = chr(random.randint(0, 32000)%90 + 32)
-    string_individual = string_individual[0:ipos] + rchar + string_individual[(ipos + 1): ]
+    string_individual = string_individual[0:ipos] + rchar + string_individual[(ipos + 1):]
 
     return string_individual
 
@@ -58,23 +57,65 @@ def crossover(p1_string, p2_string):
 def genetic_optimizer(init_population, elit_percent, max_iteration,
                       mutation_probability):
 
-    pass
+    initialized_population = initialized_population(init_population)
+    original_population_size = len(initialized_population)
+    
+    top_elite = int(elit_percent*original_population_size)
+    population = initialized_population
+    
+    for i in range(max_iteration):
 
+        individual_scores = [(string_fitness(v), v)
+                             for v in population]
+    
+        individual_scores.sort()
 
+        ranked_individuals = [v for (s, v) in individual_scores]
 
+        population = ranked_individuals[0:top_elite]
+
+        while len(population) < original_population_size:
+
+            if random.random() < mutation_probability:
+
+                c = random.randint(0, top_elite)
+                population.append(mutate_string(ranked_individuals[c]))
+                            
+            else:
+
+                c1 = random.randint(0, top_elite)
+                c2 = random.randint(0, top_elite)
+
+                population.append(crossover(ranked_individuals[c1],
+                                            ranked_individuals[c2]))
+
+        if individual_scores[0][0] == 0:
+            return individual_scores[0][1]
+
+    return individual_scores[0][1]
 
 
 def get_args():
 
     parser = argparse.ArgumentParser('')
 
-    parser.add_argument('')
-    parser.add_argument('')
-
-
+    parser.add_argument('--init_population_size', type=int,
+                        default=100)
+    parser.add_argument('--mutation_percentage', type=float,
+                        default=0.25)
+    parser.add_argument('--elite_percentage', type=float,
+                        default=0.1)
+    parser.add_argument('--max_iteration', type=int,
+                        default=100)
+    
     args = parser.parse_args()
     return args
 
 if __name__ == '__main__':
 
     args = get_args()
+
+    genetic_optimizer(args.init_population_size, args.elit_percentage,
+                      args.max_iteration, args.mutation_percentage)
+
+    
